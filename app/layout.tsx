9a2bf1ebe52f2928be34c33bee564454524a1b33
greff379 <igor.greff@yandex.ru>
2024-08-30 14:19:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,9 @@ export default function RootLayout({
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <!-- Yandex.RTB -->
+        <script>window.yaContextCb=window.yaContextCb||[]</script>
+        <script src="https://yandex.ru/ads/system/context.js" async></script>
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
