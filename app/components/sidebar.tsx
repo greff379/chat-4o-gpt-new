471fbf15487d6f6386c2755f3f3ccd8c0b3fdfca
greff379 <igor.greff@yandex.ru>
2024-08-30 14:36:47 +0300

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -164,15 +164,14 @@ export function SideBar(props: { className?: string }) {
           <ChatGptIcon />
         </div>
 
-          <div id="yandex_rtb_R-A-11782066-1"></div>
-            <script>
-             window.yaContextCb.push(() => {
-             Ya.Context.AdvManager.render({
+         <script>
+         window.yaContextCb.push(() => {
+           Ya.Context.AdvManager.render({
             "blockId": "R-A-11782066-1",
             "renderTo": "yandex_rtb_R-A-11782066-1"
-              })
-             })
-            </script>
+           })
+         })
+         </script>
         
       </div>
    
