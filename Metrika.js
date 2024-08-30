6dcbe4a16617f756775e18092e27b38a44f32001
greff379 <igor.greff@yandex.ru>
2024-08-30 16:00:35 +0300

    Update Metrika.js

--- Metrika.js
+++ Metrika.js
@@ -1,10 +1,1 @@
-<!-- Yandex.RTB R-A-11782066-1 -->
-<div id="yandex_rtb_R-A-11782066-1"></div>
-<script>
-window.yaContextCb.push(() => {
-    Ya.Context.AdvManager.render({
-        "blockId": "R-A-11782066-1",
-        "renderTo": "yandex_rtb_R-A-11782066-1"
-    })
-})
-</script>
+
