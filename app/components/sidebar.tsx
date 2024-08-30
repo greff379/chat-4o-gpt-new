ace05b46f75ac4c6ac8fd256c5acfd96ea128eae
greff379 <igor.greff@yandex.ru>
2024-08-30 14:31:32 +0300

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -156,7 +156,6 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           <a href="https://chat-gpt-free.com/" target="_blank">Chat-GPT-free.com</a>
-
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Your own GPT assistant
@@ -164,6 +163,17 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
+
+          <div id="yandex_rtb_R-A-11782066-1"></div>
+            <script>
+             window.yaContextCb.push(() => {
+             Ya.Context.AdvManager.render({
+            "blockId": "R-A-11782066-1",
+            "renderTo": "yandex_rtb_R-A-11782066-1"
+              })
+             })
+            </script>
+        
       </div>
    
       <div className={styles["sidebar-header-bar"]}>
